d99fb160b49a595e8964e4ddea91d0ba9647dbc0
zpGao <beyond_acm@163.com>
2016-04-10 16:39:58 +0800

    Split data for Train & Test

--- extract_qid.py
+++ extract_qid.py
@@ -1,21 +1,28 @@
 # calculate the number of examples in the training set
 
-
 def extract_qid(PATH_IN, PATH_OUT) :
-	
-	f_in = open(PATH_IN + 'qapair.txt', 'r')
-	f_out = open(PATH_OUT + 'qid_ex.txt', 'w')
 
-	examples = 0
-	for line in f_in:
-		info = line.split(',')
-		qid = info[0]
-		num_a = info[-1]
-		if int(num_a) >= 3:
-			print num_a
-			examples = examples + 1        
-			f_out.write(qid+'\n')
-			print qid
-			
-	f_in.close()
-	f_out.close()
+    #f_in = open(PATH_IN + 'qapair.txt', 'r')
+    #f_out = open(PATH_OUT + 'qid_ex.txt', 'w')    
+    
+    with open(PATH_IN + 'qapair.txt', 'r') as f_in, \
+    open(PATH_OUT + 'qid_more_3', 'w') as f_out1,     \
+    open(PATH_OUT + 'qid_less_3', 'w') as f_out2:
+        examples = 0
+        for line in f_in:
+
+            info = line.split(',')
+            qid = info[0]
+            num_answer = info[-1]
+
+            if int(num_answer) >= 3:
+                # print num_a
+                # examples = examples + 1        
+                    f_out1.write(qid+'\n')
+                # print qid`
+                else :
+                    f_out2.write(qid+'\n')
+
+
+                    #f_in.close()
+                    #f_out.close()
--- load.py
+++ load.py
@@ -14,69 +14,69 @@ def str_strip(data) :
     return data  
 
 def format_data(PATH_IN, PATH_OUT, FILE):
-	
-	f1 = open(PATH_IN + 'answer.json')
-	f2 = open(PATH_IN + 'question_filtered.json')
-	f3 = open(PATH_IN + 'user.json')
-	f4 = open(PATH_IN + 'qtags.json')
 
-	answer = [json.loads(line.strip().strip(',')) for line in f1]
-	#print answer[0]
-	#answer_frame 
-	answer_frame = DataFrame(answer)
-	answer_frame = answer_frame.drop_duplicates('answer_id')
-	#print answer_frame #14781 x 5
+    f1 = open(PATH_IN + 'answer.json')
+    f2 = open(PATH_IN + 'question_filtered.json')
+    f3 = open(PATH_IN + 'user.json')
+    f4 = open(PATH_IN + 'qtags.json')
 
-	question = [json.loads(line.strip().strip(',')) for line in f2]
-	#print question[0]
-	question_frame = DataFrame(question)
-	question_frame = question_frame.drop_duplicates('question_id')
-	#print question_frame  # 898 x 2
+    answer = [json.loads(line.strip().strip(',')) for line in f1]
+    #print answer[0]
+    #answer_frame 
+    answer_frame = DataFrame(answer)
+    answer_frame = answer_frame.drop_duplicates('answer_id')
+    #print answer_frame #14781 x 5
 
-	user = [json.loads(line.strip().strip(',')) for line in f3]
-	user_frame = DataFrame(user)
-	user_frame = user_frame.drop_duplicates('user_id')
-	#print user_frame    # 3378 x 14
+    question = [json.loads(line.strip().strip(',')) for line in f2]
+    #print question[0]
+    question_frame = DataFrame(question)
+    question_frame = question_frame.drop_duplicates('question_id')
+    #print question_frame  # 898 x 2
 
-	qtag = [json.loads(line.strip().strip(',')) for line in f4]
-	qtag_frame = DataFrame(qtag)
-	qtag_frame = qtag_frame.drop_duplicates('question_id')
-	#print qtag_frame      # 9999 x 2
+    user = [json.loads(line.strip().strip(',')) for line in f3]
+    user_frame = DataFrame(user)
+    user_frame = user_frame.drop_duplicates('user_id')
+    #print user_frame    # 3378 x 14
 
-	# Generate the source data
-	m1 = pd.merge(answer_frame, user_frame, how = 'outer')
-	m1 = m1.rename(columns = {'related_id':'question_id'})
-	#print m1.head()    # 14781 x 18
+    qtag = [json.loads(line.strip().strip(',')) for line in f4]
+    qtag_frame = DataFrame(qtag)
+    qtag_frame = qtag_frame.drop_duplicates('question_id')
+    #print qtag_frame      # 9999 x 2
 
-	m2 = pd.merge(qtag_frame, question_frame)
-	#print m2.head()     # 898 x 3 
+    # Generate the source data
+    m1 = pd.merge(answer_frame, user_frame, how = 'outer')
+    m1 = m1.rename(columns = {'related_id':'question_id'})
+    #print m1.head()    # 14781 x 18
 
-	# Generate 
-	m = pd.merge(m1, m2, on = 'question_id')
-	#print m
-	m = m.sort(['question_id','answer_voted'], ascending=[1,0])
-	m = m.drop('user_address', axis=1)
-	m = m.drop('user_name', axis=1)
-	m = m.reset_index(drop=True)
-	#print m
+    m2 = pd.merge(qtag_frame, question_frame)
+    #print m2.head()     # 898 x 3 
 
-	# modify the string in dataframe m
-	m['answer_content'] = str_strip(m['answer_content'])
-	m['question_content'] = str_strip(m['question_content'])
-	m['user_edu'] = str_strip(m['user_edu'])
-	m['user_specialty'] = str_strip(m['user_specialty'])
-	m['user_interest'] = str_strip(m['user_interest'].astype(str))
-	m['user_intro'] = str_strip(m['user_intro'])
-	m['question_tags'] = str_strip(m['question_tags'].astype(str))
-	m['user_recommends'] = str_strip(m['user_recommends'].astype(str))
-	print m      # 3195 x 20
+    # Generate 
+    m = pd.merge(m1, m2, on = 'question_id')
+    #print m
+    m = m.sort(['question_id','answer_voted'], ascending=[1,0])
+    m = m.drop('user_address', axis=1)
+    m = m.drop('user_name', axis=1)
+    m = m.reset_index(drop=True)
+    #print m
 
-	#m.to_json(PATH_OUT + 'total01.json')
-	
-	m.to_csv(PATH_OUT + FILE + '.csv', encoding='utf-8', index = False)
+    # modify the string in dataframe m
+    m['answer_content'] = str_strip(m['answer_content'])
+    m['question_content'] = str_strip(m['question_content'])
+    m['user_edu'] = str_strip(m['user_edu'])
+    m['user_specialty'] = str_strip(m['user_specialty'])
+    m['user_interest'] = str_strip(m['user_interest'].astype(str))
+    m['user_intro'] = str_strip(m['user_intro'])
+    m['question_tags'] = str_strip(m['question_tags'].astype(str))
+    m['user_recommends'] = str_strip(m['user_recommends'].astype(str))
+    print m      # 3195 x 20
 
-	f1.close()
-	f2.close()
-	f3.close()
-	f4.close()
+    #m.to_json(PATH_OUT + 'total01.json')
+
+    m.to_csv(PATH_OUT + FILE + '.csv', encoding='utf-8', index = False)
+
+    f1.close()
+    f2.close()
+    f3.close()
+    f4.close()
 
