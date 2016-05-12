c0c5782b271ec5368a4e5f944a0dc21de8ffbc5e
zpGao <beyond_acm@163.com>
2016-05-13 01:07:11 +0800

    format zhihu data

--- S2V_Model/demo.py
+++ S2V_Model/demo.py
@@ -18,7 +18,7 @@ logging.info("running %s" % " ".join(sys.argv))
 
 input_file = 'TEXT_SOURCE.csv'
 model = Word2Vec(LineSentence(input_file), size=50, window=5, sg=0, min_count=5, workers=8)
-model.save(input_file + '.model')
+model.save('S2V.model')
 model.save_word2vec_format(input_file + '.vec')
 
 # sent_file = 'TOTAL_LDA_SOURCE.txt'
